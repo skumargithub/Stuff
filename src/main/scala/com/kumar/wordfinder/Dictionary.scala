5d428fef9ac96fcd7e8f3cc248dd27e8a04b763d
Senthil Kumar <skumaremail@yahoo.com>
2017-03-03 15:54:11 -0700

    Consume the words in a .gzip format

--- src/main/scala/com/kumar/wordfinder/Dictionary.scala
+++ src/main/scala/com/kumar/wordfinder/Dictionary.scala
@@ -1,11 +1,15 @@
 package com.kumar.wordfinder
 
+import java.io.FileInputStream
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+
 object Dictionary {
-  val bs = scala.io.Source.fromFile("words.txt")
+  val bs = scala.io.Source.fromInputStream(new GzipCompressorInputStream(new FileInputStream("words.txt.gz")))
   val set = scala.collection.mutable.HashSet[String]()
   for(line <- bs.getLines) {
     set.add(line.toLowerCase)
   }
+  bs.close
   
   println(s"Finished reading words, total: ${set.size}")
   
